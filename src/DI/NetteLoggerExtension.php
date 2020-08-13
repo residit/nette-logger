351bf8fd5c1a8eb75876bb7a63f836fc25cb7d9d
Jan Vaníček <janvanicek@post.cz>
2020-08-13 15:23:14 +0200

    Refactor

--- src/DI/NetteLoggerExtension.php
+++ src/DI/NetteLoggerExtension.php
@@ -4,79 +4,82 @@ declare(strict_types=1);
 
 namespace Residit\NetteLogger\DI;
 
+use Nette;
+use Nette\Schema\Expect;
 use Nette\DI\CompilerExtension;
-use Tracy\Debugger;
-use Tracy\ILogger;
+use Nette\PhpGenerator\ClassType;
+use Residit\NetteLogger\NetteLogger;
 
 class NetteLoggerExtension extends CompilerExtension
 {
-    private const PARAM_TOKEN = 'token';
-    private const PARAM_USER_DATA = 'user_data';
+  /**
+   * @var string $prefix
+   */
+  private $prefix;
 
-    private $defaults = [
-        self::PARAM_TOKEN => null,
-        self::PARAM_USER_DATA => [],
-    ];
+  /**
+   * Check configuration of extension
+   * @return Nette\Schema\Schema
+   */
+  public function getConfigSchema(): Nette\Schema\Schema
+  {
+    return Expect::structure([
+      'url' => Expect::string(),
+      'token' => Expect::string(),
+      'userData' => Expect::mixed()->nullable()
+    ]);
+  }
 
-    private $enabled = false;
+  /**
+   * Load configuration of extension
+   */
+  public function loadConfiguration()
+  {
+    $this->prefix = 'logger';
+    $builder = $this->getContainerBuilder();
 
-    public function loadConfiguration()
-    {
-        $this->validateConfig($this->defaults);
+    $builder->addDefinition($this->prefix($this->prefix))
+      ->setFactory(NetteLogger::class)
+      ->addSetup(
+        'register', []
+      )->addSetup(
+        'setUrl', [$this->config->url]
+      )->addSetup(
+        'setToken', [$this->config->token]
+      )->addSetup(
+        'setUserData', [$this->config->userData]
+      );
+  }
 
-        if (!$this->config[self::PARAM_TOKEN]) {
-            Debugger::log('Unable to initialize NetteLogger, token config option is missing', ILogger::WARNING);
-            return;
-        }
+  /**
+   * Pass data to extension before compiling to PHP class
+   */
+  public function beforeCompile()
+  {
+    $builder = $this->getContainerBuilder();
 
-        $this->enabled = true;
-
-        $this->getContainerBuilder()
-            ->addDefinition($this->prefix('netteLogger'))
-            ->setFactory(\Residit\NetteLogger\NetteLogger::class)
-            ->addSetup(
-                'register',
-                [
-                ]
-            )->addSetup(
-                'setUserData',
-                [
-                    $this->config[self::PARAM_USER_DATA],
-                ]
-            )->addSetup(
-                'setToken',
-                [
-                    $this->config[self::PARAM_TOKEN],
-                ]
-            );
+    if ($builder->hasDefinition('tracy.logger')) {
+      $builder->getDefinition('tracy.logger')->setAutowired(false);
     }
 
-    public function beforeCompile()
-    {
-        if (!$this->enabled) return;
-
-        $builder = $this->getContainerBuilder();
-
-        if ($builder->hasDefinition('tracy.logger')) {
-            $builder->getDefinition('tracy.logger')->setAutowired(false);
-        }
-
-        if ($builder->hasDefinition('security.user')) {
-            $builder->getDefinition($this->prefix('netteLogger'))
-                ->addSetup('setUser', [$builder->getDefinition('security.user')]);
-        }
-
-        if ($builder->hasDefinition('session.session')) {
-            $builder->getDefinition($this->prefix('netteLogger'))
-                ->addSetup('setSession', [$builder->getDefinition('session.session')]);
-        }
+    if ($builder->hasDefinition('security.user')) {
+      $builder->getDefinition($this->prefix($this->prefix))
+        ->addSetup('setIdentity', [$builder->getDefinition('security.user')]);
     }
 
-    public function afterCompile(\Nette\PhpGenerator\ClassType $class)
-    {
-        if (!$this->enabled) return;
-
-        $class->getMethod('initialize')
-            ->addBody('Tracy\Debugger::setLogger($this->getService(?));', [ $this->prefix('netteLogger') ]);
+    if ($builder->hasDefinition('session.session')) {
+      $builder->getDefinition($this->prefix($this->prefix))
+        ->addSetup('setSession', [$builder->getDefinition('session.session')]);
     }
+  }
+
+  /**
+   * Initializing logger class after compiling into PHP class
+   * @param ClassType $class
+   */
+  public function afterCompile(ClassType $class)
+  {
+    $class->getMethod('initialize')
+      ->addBody('Tracy\Debugger::setLogger($this->getService(?));', [$this->prefix($this->prefix)]);
+  }
 }
--- src/NetteLogger.php
+++ src/NetteLogger.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Residit\NetteLogger;
 
 use Nette\Http\Session;
-use Nette\Security\IIdentity;
+use Nette\Security\Identity;
 use Nette\Security\User;
 use Nette\Utils\Json;
 use Tracy\Debugger;
@@ -14,93 +14,146 @@ use Tracy\Logger;
 
 class NetteLogger extends Logger
 {
-    /** @var IIdentity */
-    private $identity;
+  /**
+   * @var Identity $identity
+   */
+  private $identity;
 
-    /** @var Session */
-    private $session;
+  /**
+   * @var Session $session
+   */
+  private $session;
 
-    /** @var array */
-    private $userData = [];
+  /**
+   * @var string $url
+   */
+  private $url;
 
-    /** @var string */
-    private $token = null;
+  /**
+   * @var null $token
+   */
+  private $token = null;
 
-    public function register()
-    {
-        $this->directory = Debugger::$logDirectory;
+  /**
+   * @var array $userData
+   */
+  private $userData = [];
+
+  public function setIdentity(User $user)
+  {
+    $this->identity = $user->getIdentity();
+  }
+
+  public function setSession(Session $session)
+  {
+    $this->session = $session;
+  }
+
+  public function setUrl(string $url)
+  {
+    $this->url = $url;
+  }
+
+  public function setToken(string $token)
+  {
+    $this->token = $token;
+  }
+
+  public function setUserData(array $userData)
+  {
+    $this->userData = $userData;
+  }
+
+  /**
+   * Init logging directory
+   */
+  public function register()
+  {
+    $this->directory = Debugger::$logDirectory;
+  }
+
+  /**
+   * Process the error and log it
+   *
+   * @param mixed $value
+   * @param string $priority
+   * @return bool|string|null
+   * @throws \Nette\Utils\JsonException
+   */
+  public function log($value, $priority = ILogger::INFO)
+  {
+    $response = parent::log($value, $priority);
+    $userData = null;
+    $sessionData = null;
+
+    if ($this->identity) {
+      $userData = ['id' => $this->identity->getId()];
+
+      foreach ($this->userData as $name) {
+        $userData[$name] = $this->identity->{$name} ?? null;
+      }
     }
 
-    public function setUser(User $user)
-    {
-        $this->identity = $user->getIdentity();
-    }
-
-    public function setSession(Session $session)
-    {
-      $this->session = $session;
-    }
-
-    public function setUserData(array $userData)
-    {
-        $this->userData = $userData;
-    }
-
-    public function setToken(string $token)
-    {
-        $this->token = $token;
-    }
-
-    public function log($value, $priority = ILogger::INFO)
-    {
-        $response = parent::log($value, $priority);
-        $userData = null;
-        $sessionData = null;
-
-        if ($this->identity) {
-            $userData = [
-                'id' => $this->identity->getId(),
-            ];
-
-            foreach ($this->userData as $name) {
-                $userData[$name] = $this->identity->{$name} ?? null;
-            }
+    if ($this->session) {
+      foreach ($this->session->getIterator() as $section) {
+        foreach ($this->session->getSection($section)->getIterator() as $key => $val) {
+          $sessionData[$section][$key] = $val;
         }
-
-        if ($this->session) {
-            foreach ($this->session->getIterator() as $section) {
-                foreach ($this->session->getSection($section)->getIterator() as $key => $val) {
-                    $sessionData[$section][$key] = $val;
-                }
-            }
-        }
-
-        $htmlWithoutScriptTags = preg_replace('#<script(.*?)>(.*?)</script>#is', '', file_get_contents($response));
-
-        $json = array(
-          'title' => $value->getMessage(),
-          'type' => $priority,
-          'url' => $value->getFile(),
-          'trace' => Json::encode($value->getTrace()),
-          'line' => $value->getLine(),
-          'session' => Json::encode($sessionData),
-          'user' => Json::encode($userData),
-          'html' => $htmlWithoutScriptTags,
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"http://log.residit.loc/api/v1/log");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-Auth-Token: ' . $this->token]);
-
-        $response = curl_exec ($ch);
-
-        curl_close ($ch);
-
-        bdump($response);
-
-        return $response;
+      }
     }
+
+    $htmlWithoutScriptTags = preg_replace('#<script(.*?)>(.*?)</script>#is', '', file_get_contents($response));
+
+    $logData = array(
+      'title' => $value->getMessage(),
+      'type' => $priority,
+      'url' => $value->getFile(),
+      'trace' => Json::encode($value->getTrace()),
+      'line' => $value->getLine(),
+      'session' => Json::encode($sessionData),
+      'user' => Json::encode($userData),
+      'html' => $htmlWithoutScriptTags,
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $logData);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-Auth-Token: ' . $this->token]);
+
+    $response = curl_exec($ch);
+    $responseHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+
+    $this->checkResponse($responseHttpCode, $response);
+
+    return $response;
+  }
+
+  /**
+   * Handle response states
+   *
+   * @param $httpCode
+   * @param $response
+   * @throws \Nette\Utils\JsonException
+   */
+  public function checkResponse($httpCode, $response)
+  {
+    switch ($httpCode):
+      case 401:
+      case 201:
+        $response = Json::decode($response)->status;
+        $this->logStatusToConsole($response);
+        break;
+      default:
+        $this->logStatusToConsole('Sending to API error, check API endpoint or url in config file!');
+    endswitch;
+  }
+
+  public function logStatusToConsole($text)
+  {
+    echo("<script>console.log('%cNette Logger:', 'background: #d50000; color: #fff;', ' " . $text . "')</script>");
+  }
 }
